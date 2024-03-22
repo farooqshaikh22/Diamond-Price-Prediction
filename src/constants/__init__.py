0f6b774cfb820514920eaf5c0693f0b49bbb1941
Faruq <farooqshaikh22@ymail.com>
2024-03-22 07:30:30 +0530

    data ingestion configuration done

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -6,14 +6,15 @@ def current_time_stamp():
 
 CURRENT_TIME_STAMP = current_time_stamp()
 
-ROOT_DIR = os.getcwd()
+ROOT_DIR_KEY = os.getcwd()
 DATA_DIR = 'data'
+DATA = 'gemstone.csv'
 ARTIFACT_DIR_KEY = 'Artifact'
 
 ## data ingestion related variables
 DATA_INGESTION_KEY = 'data_ingestion'
-ROW_DATA_DIR_KEY = 'raw_data_dir'
+RAW_DATA_DIR_KEY = 'raw_data_dir'
 INGESTED_DATA_DIR_KEY = 'ingested_data_dir'
-ROW_DATA = 'gemstone.csv'
+RAW_DATA = 'row.csv'
 TRAIN_DATA = 'train.csv'
 TEST_DATA = 'test.csv'
