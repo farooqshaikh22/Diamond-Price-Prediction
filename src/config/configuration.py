90898321f74b87f2f7752a6567ee6d712eb54dcf
Faruq <farooqshaikh22@ymail.com>
2024-03-23 22:48:59 +0530

    data transformation configuration done

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -4,7 +4,8 @@ import os,sys
 ROOT_DIR = ROOT_DIR_KEY
 
 DATASET_PATH = os.path.join(ROOT_DIR,DATA_DIR,DATA)
- 
+
+## data ingestion 
 RAW_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,DATA_INGESTION_KEY,CURRENT_TIME_STAMP,
                              RAW_DATA_DIR_KEY,RAW_DATA)
 
@@ -13,3 +14,24 @@ TRAIN_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,DATA_INGESTION_KEY,CURR
 
 TEST_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,DATA_INGESTION_KEY,CURRENT_TIME_STAMP,
                               INGESTED_DATA_DIR_KEY,TEST_DATA)
+
+## data transformation
+
+DATA_TRANSFORMED_PROCESSOR_OBJ_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,
+                                                        DATA_TRANSFORMATION_ARTIFACT_KEY,
+                                                        DATA_PROCESSOR_DIR,
+                                                        DATA_TRANSFORMED_PROCESSOR_OBJ)
+
+FEATURE_ENGINEERING_OBJ_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,
+                                                 DATA_TRANSFORMATION_ARTIFACT_KEY,
+                                                 DATA_PROCESSOR_DIR,FEATURE_ENGINEERING_OBJ)
+
+TRANSFORMED_TRAIN_DATA_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,
+                                                DATA_TRANSFORMATION_ARTIFACT_KEY,
+                                                DATA_TRANSFORMATION_DIR,
+                                                TRANSFORMED_TRAIN_DATA)
+
+TRANSFORMED_TEST_DATA_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,
+                                               DATA_TRANSFORMATION_ARTIFACT_KEY,
+                                               DATA_TRANSFORMATION_DIR,
+                                               TRANSFORMED_TEST_DATA)
