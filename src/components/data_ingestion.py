1d7f5e6cd99ca327c6741ab70bec24d038a19b1c
Faruq <farooqshaikh22@ymail.com>
2024-03-24 09:58:35 +0530

    train_arr n test_arr defined in data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from src.logger import logging
 from src.exception import CustomException
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
@@ -40,10 +41,14 @@ class DataIngestion:
             )
             
         except Exception as e:
-            custom_exception =  CustomException(e,sys)
-            logging.info(custom_exception)
+            logging.info('Exception occured in initiate_data_ingestion')
+            raise CustomException(e,sys)
+            
         
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data,test_data = obj.initiate_data_ingestion()
+    train_data_path,test_data_path = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    train_arr,test_arr = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
