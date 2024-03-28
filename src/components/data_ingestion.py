409114180af972a84f90f9ed65e4e121873eb384
Faruq <farooqshaikh22@ymail.com>
2024-03-28 08:52:12 +0530

    model training pipeline added in data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from src.logger import logging
 from src.exception import CustomException
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import Modeltrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -52,3 +53,7 @@ if __name__ == "__main__":
     
     data_transformation = DataTransformation()
     train_arr,test_arr = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    
+    model_trainer = Modeltrainer()
+    print(model_trainer.initiate_model_training(train_arr,test_arr))
+    
