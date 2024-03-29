84987a427d831f2f9e25bcba31325f406c680200
Faruq <farooqshaikh22@ymail.com>
2024-03-29 08:11:54 +0530

    training_pipeline.py code done

---
+++ src/pipeline/training_pipeline.py
@@ -0,0 +1,25 @@
+from src.constants import *
+from src.config.configuration import *
+import pandas as pd
+import os,sys
+from src.logger import logging
+from src.exception import CustomException
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import Modeltrainer
+from src.components.data_ingestion import DataIngestion
+
+class Train:
+    def __init__(self):
+        self.c = 0
+        print(f"***********************{self.c}**************************")
+        
+        
+    def main(self):
+        obj = DataIngestion()
+        train_data_path,test_data_path = obj.initiate_data_ingestion()
+        
+        data_transformation = DataTransformation()
+        train_arr,test_arr = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        
+        model_trainer = Modeltrainer()
+        print(model_trainer.initiate_model_training())
