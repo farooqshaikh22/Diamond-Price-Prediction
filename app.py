0157a9fdfe16d946e4b3b6372240453840b15985
Faruq <farooqshaikh22@ymail.com>
2024-04-01 15:34:52 +0530

    app.py completed

--- app.py
+++ app.py
@@ -49,7 +49,67 @@ def predict_datapoint():
         
         return render_template('form.html',final_result = result)
 
+@app.route("/batch",methods = ["GET","POST"])
+def perform_batch_prediction():
+    if request.method == "GET":
+        return render_template('batch.html')
+    
+    else:
+        file = request.files['csv_file']  # Update the key to 'csv_file'
+        # Directory path
+        directory_path = UPLOAD_FOLDER
+        # Create the directory
+        os.makedirs(directory_path, exist_ok=True)
 
+        # Check if the file has a valid extension
+        if file and '.' in file.filename and file.filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
+            # Delete all files in the file path
+            for filename in os.listdir(os.path.join(UPLOAD_FOLDER)):
+                file_path = os.path.join(UPLOAD_FOLDER, filename)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+
+            # Save the new file to the uploads directory
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(UPLOAD_FOLDER, filename)
+            file.save(file_path)
+            print(file_path)
+
+            logging.info("CSV received and Uploaded")
+            
+            # Perform batch prediction using the uploaded file
+            
+            batch = batch_prediction(file_path,
+                             model_file_path,
+                             transformer_file_path)
+            
+            batch.start_batch_prediction()
+            
+            output = "Batch Prediction Done"
+            
+            return render_template("batch.html", prediction_result=output, prediction_type='batch')
+        else:
+            return render_template('batch.html', prediction_type='batch', error='Invalid file type')
+        
+        
+@app.route("/train",methods = ["GET","POST"])
+def train():
+    if request.method == "GET":
+        return render_template('train.html')
+    
+    else:
+        try:
+        
+            pipeline = Train()
+            
+            pipeline.main()
+            
+            return render_template('train.html',message = 'Training Complete')
+        
+        except Exception as e:
+            logging.error(f"{e}")
+            error_message = str(e)
+            return render_template('index.html',error = error_message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host ='0.0.0.0',debug=True)
