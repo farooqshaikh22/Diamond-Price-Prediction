b3320a917d2f690e66ff3bfc3636b6374f3c60e2
Faruq <farooqshaikh22@ymail.com>
2024-04-18 16:27:53 +0530

    setup.py corrected

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import find_packages,setup
+from typing import List
+
+HYPHON_E_DOT = "-e ."
+
+def get_requirements(filepath:str) -> List[str]:
+      requirements = []
+      
+      with open(filepath) as file_obj:
+            requirements = file_obj.readlines()
+            requirements = [i.replace("\n","") for i in requirements]
+            
+            if HYPHON_E_DOT in requirements:
+                  requirements.remove(HYPHON_E_DOT)
 
 setup(
       name='Diamond Price Prediction',
-      version= '0.0.0',
+      version= '0.0.1',
       author= 'Faruq Shaikh',
       author_email='farooqshaikh22@ymail.com',
       packages=find_packages(),
-      install_requires = []
+      install_requires = get_requirements("requirements.txt")
       )
